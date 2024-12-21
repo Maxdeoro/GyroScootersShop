2fb4b43d91f132a8c9814d00f69de6f93ccda3aa
Maxdeoro <maxdeoro@gmail.com>
2024-12-21 22:30:22 +0300

    show more images

--- imgView.js
+++
@@ -1,9 +0,0 @@
-function bigImg(x) {
-    x.style.height = "240px";
-    x.style.width = "192px";
-  }
-  
-  function normalImg(x) {
-    x.style.height = "200px";
-    x.style.width = "160px";
-  }
--- menu.js
+++
@@ -1,3 +0,0 @@
-(function($){
-    $('.jquerynav').jQueryNav();
-})(jQuery);
